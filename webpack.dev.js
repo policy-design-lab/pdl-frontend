01ee89da74e6c0d1159fbb72132abda2644793c5
Chris Navarro <cmnavarr@illinois.edu>
2023-01-26 10:08:03 -0600

    Fixes #24 - Debugger was not evaluating breakpoints

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,7 @@ const commonConfig = require('./webpack.common');
 
 module.exports = merge(commonConfig, {
     mode: 'development',
+    devtool: 'eval-source-map',
 
     devServer: {
         hot: true,
