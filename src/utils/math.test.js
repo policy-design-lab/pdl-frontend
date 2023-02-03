9d51abfd9ab01fa9dcd35a880b8a23ac1f0387ba
Yong Wook Kim <ywkim@illinois.edu>
2023-02-03 14:42:41 -0600

    added suprss warning to fix build error

--- src/utils/math.test.js
+++ src/utils/math.test.js
@@ -2,6 +2,7 @@ import { sum } from './math';
 
 describe('sum function should', () => {
     test('return sum of an array of numbers', () => {
+        // eslint-disable-next-line no-undef
         expect(sum([1, 2])).toBe(3);
     });
 });
