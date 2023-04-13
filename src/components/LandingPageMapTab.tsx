e806587101eac7addaca5df382af722527f83508
Ziyue Zhou <ziyue5@illinois.edu>
2023-04-13 09:57:32 -0500

    Display 0% and re-do eslint format

--- src/components/LandingPageMapTab.tsx
+++ src/components/LandingPageMapTab.tsx
@@ -1,25 +1,25 @@
-import * as React from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-import { CardMedia, createTheme, styled, Typography, ThemeProvider } from '@mui/material';
-import Divider from '@mui/material/Divider';
-import LandingPageMap from './LandingPageMap';
-import AllProgramMap from './AllProgramMap';
-import allPrograms from '../data/allPrograms.json';
-import commodities from '../images/legends/Commodities benefits 2018 - 2022.png';
-import conservation from '../images/legends/conservation programs benefits 2018 - 2022.png';
-import crop from '../images/legends/crop insurance 2018 - 2022.png';
-import snap from '../images/legends/SNAP programs benefits 2018 - 2022.png';
-import total from '../images/legends/total farm bill benefits 2018 - 2022.png';
-import LandingDisplay from './LandingDisplay';
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import { CardMedia, createTheme, styled, Typography, ThemeProvider } from "@mui/material";
+import Divider from "@mui/material/Divider";
+import LandingPageMap from "./LandingPageMap";
+import AllProgramMap from "./AllProgramMap";
+import allPrograms from "../data/allPrograms.json";
+import commodities from "../images/legends/Commodities benefits 2018 - 2022.png";
+import conservation from "../images/legends/conservation programs benefits 2018 - 2022.png";
+import crop from "../images/legends/crop insurance 2018 - 2022.png";
+import snap from "../images/legends/SNAP programs benefits 2018 - 2022.png";
+import total from "../images/legends/total farm bill benefits 2018 - 2022.png";
+import LandingDisplay from "./LandingDisplay";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#2F7164'
-        }
-    }
+	palette: {
+		primary: {
+			main: "#2F7164"
+		}
+	}
 });
 
 interface TabPanelProps {
@@ -29,153 +29,153 @@ interface TabPanelProps {
 }
 
 function TabPanel(props: TabPanelProps) {
-    const { value, index, title, ...other } = props;
-    let colorLegend = null;
+	const { value, index, title, ...other } = props;
+	let colorLegend = null;
 
-    switch (title) {
-        case 'Title I: Commodities':
-            colorLegend = commodities;
-            break;
-        case 'Title II: Conservation':
-            colorLegend = conservation;
-            break;
-        case 'Crop Insurance':
-            colorLegend = crop;
-            break;
-        case 'Supplemental Nutrition Assistance Program (SNAP)':
-            colorLegend = snap;
-            break;
-        case 'All Programs':
-            colorLegend = total;
-            break;
-    }
+	switch (title) {
+	case "Title I: Commodities":
+		colorLegend = commodities;
+		break;
+	case "Title II: Conservation":
+		colorLegend = conservation;
+		break;
+	case "Crop Insurance":
+		colorLegend = crop;
+		break;
+	case "Supplemental Nutrition Assistance Program (SNAP)":
+		colorLegend = snap;
+		break;
+	case "All Programs":
+		colorLegend = total;
+		break;
+	}
 
-    return (
-        <Box role="tabpanel" hidden={value !== index} {...other}>
-            {value === index && (
-                <Box
-                    sx={{
-                        width: '60%',
-                        mx: 'auto'
-                    }}
-                >
-                    <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
-                        <CardMedia sx={{ maxWidth: 720, mt: 3 }} component="img" src={colorLegend} />
-                    </Box>
-                    {title === 'All Programs' ? <AllProgramMap /> : <LandingPageMap programTitle={title} />}
-                </Box>
-            )}
-            {value === index && (
-                <Box sx={{ width: '100%' }}>
-                    {' '}
-                    <LandingDisplay programTitle={title} />{' '}
-                </Box>
-            )}
-        </Box>
-    );
+	return (
+		<Box role="tabpanel" hidden={value !== index} {...other}>
+			{value === index && (
+				<Box
+					sx={{
+						width: "60%",
+						mx: "auto"
+					}}
+				>
+					<Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
+						<CardMedia sx={{ maxWidth: 720, mt: 3 }} component="img" src={colorLegend} />
+					</Box>
+					{title === "All Programs" ? <AllProgramMap /> : <LandingPageMap programTitle={title} />}
+				</Box>
+			)}
+			{value === index && (
+				<Box sx={{ width: "100%" }}>
+					{" "}
+					<LandingDisplay programTitle={title} />{" "}
+				</Box>
+			)}
+		</Box>
+	);
 }
 
 export default function LandingPageMapTab(): JSX.Element {
-    const [value, setValue] = React.useState(2);
+	const [value, setValue] = React.useState(2);
 
-    const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-    };
+	const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+		setValue(newValue);
+	};
 
     interface StyledTabProps {
         label: JSX.Element;
     }
 
     const CustomTab = styled((props: StyledTabProps) => <Tab disableRipple {...props} />)({
-        textTransform: 'none'
+    	textTransform: "none"
     });
 
-    const cur = allPrograms.find((s) => s.State === 'Total');
-    const allProgramTotal = cur['18-22 All Programs Total'];
-    const titleITotal = cur['Title I Total'];
-    const titleIITotal = cur['Title II Total'];
-    const cropTotal = cur['Crop Insurance Total'];
-    const snapTotal = cur['SNAP Total'];
+    const cur = allPrograms.find((s) => s.State === "Total");
+    const allProgramTotal = cur["18-22 All Programs Total"];
+    const titleITotal = cur["Title I Total"];
+    const titleIITotal = cur["Title II Total"];
+    const cropTotal = cur["Crop Insurance Total"];
+    const snapTotal = cur["SNAP Total"];
 
     return (
-        <Box sx={{ width: '100%', mt: 5 }}>
-            <Box display="flex" justifyContent="center" width="100%" sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <ThemeProvider theme={theme}>
-                    <Tabs
-                        TabIndicatorProps={{ style: { background: '#2F7164' } }}
-                        variant="scrollable"
-                        value={value}
-                        onChange={handleChange}
-                        centered
-                        className="tab"
-                    >
-                        <Box sx={{ mt: 1.5, pb: 0, mr: 8 }}>
-                            <Typography variant="h4" className="smallCaps">
-                                <strong>Farm Bill Data</strong>
-                            </Typography>
-                            <Typography variant="h6" className="allSmallCaps" sx={{ mt: 1 }}>
-                                <strong>Map visualization</strong>
-                            </Typography>
-                        </Box>
-                        <Divider sx={{ mx: 1 }} orientation="vertical" variant="middle" flexItem />
-                        <CustomTab
-                            label={
-                                <Box>
-                                    <Typography>All Programs</Typography>
-                                    <br />
-                                    <Typography>${Number(allProgramTotal / 1000000000.0).toFixed(2)}B</Typography>
-                                </Box>
-                            }
-                        />
-                        <Divider sx={{ mx: 1 }} orientation="vertical" variant="middle" flexItem />
-                        <CustomTab
-                            label={
-                                <Box>
-                                    <Typography>Title I: Commodities</Typography>
-                                    <br />
-                                    <Typography>${Number(titleITotal / 1000000000.0).toFixed(2)}B</Typography>
-                                </Box>
-                            }
-                        />
-                        <Divider sx={{ mx: 1 }} orientation="vertical" variant="middle" flexItem />
-                        <CustomTab
-                            label={
-                                <Box>
-                                    <Typography>Title II: Conservation</Typography>
-                                    <br />
-                                    <Typography>${Number(titleIITotal / 1000000000.0).toFixed(2)}B</Typography>
-                                </Box>
-                            }
-                        />
-                        <Divider sx={{ mx: 1 }} orientation="vertical" variant="middle" flexItem />
-                        <CustomTab
-                            label={
-                                <Box>
-                                    <Typography>Crop Insurance</Typography>
-                                    <br />
-                                    <Typography>${Number(cropTotal / 1000000000.0).toFixed(2)}B</Typography>
-                                </Box>
-                            }
-                        />
-                        <Divider sx={{ mx: 1 }} orientation="vertical" variant="middle" flexItem />
-                        <CustomTab
-                            label={
-                                <Box>
-                                    <Typography>Supplemental Nutrition Assistance Program</Typography>
-                                    <br />
-                                    <Typography>${Number(snapTotal / 1000000000.0).toFixed(2)}B</Typography>
-                                </Box>
-                            }
-                        />
-                    </Tabs>
-                </ThemeProvider>
-            </Box>
-            <TabPanel value={value} index={0} title="All Programs" />
-            <TabPanel value={value} index={2} title="All Programs" />
-            <TabPanel value={value} index={4} title="Title I: Commodities" />
-            <TabPanel value={value} index={6} title="Title II: Conservation" />
-            <TabPanel value={value} index={8} title="Crop Insurance" />
-            <TabPanel value={value} index={10} title="Supplemental Nutrition Assistance Program (SNAP)" />
-        </Box>
+    	<Box sx={{ width: "100%", mt: 5 }}>
+    		<Box display="flex" justifyContent="center" width="100%" sx={{ borderBottom: 1, borderColor: "divider" }}>
+    			<ThemeProvider theme={theme}>
+    				<Tabs
+    					TabIndicatorProps={{ style: { background: "#2F7164" } }}
+    					variant="scrollable"
+    					value={value}
+    					onChange={handleChange}
+    					centered
+    					className="tab"
+    				>
+    					<Box sx={{ mt: 1.5, pb: 0, mr: 8 }}>
+    						<Typography variant="h4" className="smallCaps">
+    							<strong>Farm Bill Data</strong>
+    						</Typography>
+    						<Typography variant="h6" className="allSmallCaps" sx={{ mt: 1 }}>
+    							<strong>Map visualization</strong>
+    						</Typography>
+    					</Box>
+    					<Divider sx={{ mx: 1 }} orientation="vertical" variant="middle" flexItem />
+    					<CustomTab
+    						label={
+    							<Box>
+    								<Typography>All Programs</Typography>
+    								<br />
+    								<Typography>${Number(allProgramTotal / 1000000000.0).toFixed(2)}B</Typography>
+    							</Box>
+    						}
+    					/>
+    					<Divider sx={{ mx: 1 }} orientation="vertical" variant="middle" flexItem />
+    					<CustomTab
+    						label={
+    							<Box>
+    								<Typography>Title I: Commodities</Typography>
+    								<br />
+    								<Typography>${Number(titleITotal / 1000000000.0).toFixed(2)}B</Typography>
+    							</Box>
+    						}
+    					/>
+    					<Divider sx={{ mx: 1 }} orientation="vertical" variant="middle" flexItem />
+    					<CustomTab
+    						label={
+    							<Box>
+    								<Typography>Title II: Conservation</Typography>
+    								<br />
+    								<Typography>${Number(titleIITotal / 1000000000.0).toFixed(2)}B</Typography>
+    							</Box>
+    						}
+    					/>
+    					<Divider sx={{ mx: 1 }} orientation="vertical" variant="middle" flexItem />
+    					<CustomTab
+    						label={
+    							<Box>
+    								<Typography>Crop Insurance</Typography>
+    								<br />
+    								<Typography>${Number(cropTotal / 1000000000.0).toFixed(2)}B</Typography>
+    							</Box>
+    						}
+    					/>
+    					<Divider sx={{ mx: 1 }} orientation="vertical" variant="middle" flexItem />
+    					<CustomTab
+    						label={
+    							<Box>
+    								<Typography>Supplemental Nutrition Assistance Program</Typography>
+    								<br />
+    								<Typography>${Number(snapTotal / 1000000000.0).toFixed(2)}B</Typography>
+    							</Box>
+    						}
+    					/>
+    				</Tabs>
+    			</ThemeProvider>
+    		</Box>
+    		<TabPanel value={value} index={0} title="All Programs" />
+    		<TabPanel value={value} index={2} title="All Programs" />
+    		<TabPanel value={value} index={4} title="Title I: Commodities" />
+    		<TabPanel value={value} index={6} title="Title II: Conservation" />
+    		<TabPanel value={value} index={8} title="Crop Insurance" />
+    		<TabPanel value={value} index={10} title="Supplemental Nutrition Assistance Program (SNAP)" />
+    	</Box>
     );
 }
--- src/components/SemiDonutChart.tsx
+++ src/components/SemiDonutChart.tsx
@@ -1,104 +1,104 @@
-import React from 'react';
-import { PieChart, Pie, Tooltip, Label, Cell } from 'recharts';
-import { Box } from '@mui/material';
+import React from "react";
+import { PieChart, Pie, Tooltip, Label, Cell } from "recharts";
+import { Box } from "@mui/material";
 
 const RADIAN = Math.PI / 180;
 
 // eslint-disable-next-line
 export default function SemiDonutChart({ data, label1, label2 }: any): JSX.Element {
-    const data01 = data;
-    // eslint-disable-next-line
+	const data01 = data;
+	// eslint-disable-next-line
     const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }: any) => {
-        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-        const x = cx + radius * Math.cos(-midAngle * RADIAN);
-        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+		const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+		const x = cx + radius * Math.cos(-midAngle * RADIAN);
+		const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-        return (
-            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-                {`${(percent * 100).toFixed(1)}%`}
-            </text>
-        );
-    };
-    // eslint-disable-next-line
+		return (
+			<text x={x} y={y} fill="white" textAnchor={x > cx ? "start" : "end"} dominantBaseline="central">
+				{`${(percent * 100).toFixed(1)}%`}
+			</text>
+		);
+	};
+	// eslint-disable-next-line
     const renderCustomizedLabel2 = ({ x, cx, y, name }: any) => {
-        return (
-            <text x={x} y={y} fill="#000000" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-                {name}
-            </text>
-        );
-    };
+		return (
+			<text x={x} y={y} fill="#000000" textAnchor={x > cx ? "start" : "end"} dominantBaseline="central">
+				{name}
+			</text>
+		);
+	};
 
-    const CustomTooltip = ({ active, payload, label }: any) => {
-        if (active && payload && payload.length) {
-            return (
-                        <div style={{
-                            backgroundColor: '#f5f5f5',
-                            padding: '20px',
-                            borderRadius: '10px',
-                            boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)',
-                            textAlign: 'center'
-                        }}>
-                            <h2 style={{ margin: '0' }}>{payload[0].name} Payment:</h2>
-                            <p style={{ fontSize: '20px', fontWeight: 'bold' }}>{payload[0].value.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
-                        </div>
-            );
-        }
+	const CustomTooltip = ({ active, payload, label }: any) => {
+		if (active && payload && payload.length) {
+			return (
+				<div style={{
+					backgroundColor: "#f5f5f5",
+					padding: "20px",
+					borderRadius: "10px",
+					boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.1)",
+					textAlign: "center"
+				}}>
+					<h2 style={{ margin: "0" }}>{payload[0].name} Payment:</h2>
+					<p style={{ fontSize: "20px", fontWeight: "bold" }}>{payload[0].value.toLocaleString("en-US", { style: "currency", currency: "USD" })}</p>
+				</div>
+			);
+		}
 
-        return null;
-    };
+		return null;
+	};
 
-    return (
-        <Box display="flex" justifyContent="center">
-            <PieChart width={1400} height={450}>
-                <Pie
-                    dataKey="value"
-                    data={data01}
-                    startAngle={180}
-                    endAngle={0}
-                    cx={700}
-                    cy={400}
-                    innerRadius={165}
-                    outerRadius={330}
-                    fill="#82ca9d"
-                    label={renderCustomizedLabel}
-                >
-                    <Label
-                        value={
-                            Number(label1) >= 1000000000
-                                ? `$${Number(Number(label1) / 1000000000.0).toLocaleString(undefined, {
-                                      maximumFractionDigits: 2
-                                  })}B`
-                                : `$${Number(Number(label1) / 1000000.0).toLocaleString(undefined, {
-                                      maximumFractionDigits: 2
-                                  })}M`
-                        }
-                        position="center"
-                        dy={-75}
-                        style={{ textAnchor: 'middle', fontSize: '200%', fill: 'rgba(0, 0, 0, 0.87)' }}
-                    />
-                    <Label value={label2} position="center" dy={-15} />
-                    {data.map((entry) => (
-                        <Cell key={entry.id} fill={entry.color} />
-                    ))}
-                </Pie>
-                <Pie
-                    dataKey="value"
-                    data={data01}
-                    startAngle={180}
-                    endAngle={0}
-                    cx={700}
-                    cy={400}
-                    innerRadius={165}
-                    outerRadius={330}
-                    nameKey="name"
-                    label={renderCustomizedLabel2}
-                >
-                    {data.map((entry) => (
-                        <Cell key={entry.id} fillOpacity={0} />
-                    ))}
-                </Pie>
-                <Tooltip content={<CustomTooltip />} wrapperStyle={{ backgroundColor: "white", borderStyle: "ridge", outline: "none", borderRadius: '10px' }}/>
-            </PieChart>
-        </Box>
-    );
+	return (
+		<Box display="flex" justifyContent="center">
+			<PieChart width={1400} height={450}>
+				<Pie
+					dataKey="value"
+					data={data01}
+					startAngle={180}
+					endAngle={0}
+					cx={700}
+					cy={400}
+					innerRadius={165}
+					outerRadius={330}
+					fill="#82ca9d"
+					label={renderCustomizedLabel}
+				>
+					<Label
+						value={
+							Number(label1) >= 1000000000
+								? `$${Number(Number(label1) / 1000000000.0).toLocaleString(undefined, {
+									maximumFractionDigits: 2
+								})}B`
+								: `$${Number(Number(label1) / 1000000.0).toLocaleString(undefined, {
+									maximumFractionDigits: 2
+								})}M`
+						}
+						position="center"
+						dy={-75}
+						style={{ textAnchor: "middle", fontSize: "200%", fill: "rgba(0, 0, 0, 0.87)" }}
+					/>
+					<Label value={label2} position="center" dy={-15} />
+					{data.map((entry) => (
+						<Cell key={entry.id} fill={entry.color} />
+					))}
+				</Pie>
+				<Pie
+					dataKey="value"
+					data={data01}
+					startAngle={180}
+					endAngle={0}
+					cx={700}
+					cy={400}
+					innerRadius={165}
+					outerRadius={330}
+					nameKey="name"
+					label={renderCustomizedLabel2}
+				>
+					{data.map((entry) => (
+						<Cell key={entry.id} fillOpacity={0} />
+					))}
+				</Pie>
+				<Tooltip content={<CustomTooltip />} wrapperStyle={{ backgroundColor: "white", borderStyle: "ridge", outline: "none", borderRadius: "10px" }}/>
+			</PieChart>
+		</Box>
+	);
 }
