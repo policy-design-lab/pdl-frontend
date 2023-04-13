f5531b091ba91d58637a29cf7123bb688f141b4c
Ziyue Zhou <55160214+ziyue5@users.noreply.github.com>
2023-04-13 08:04:25 -0500

    Fix the tooltip styles for semi donut chart (#81)
    * Fix the tooltip styles for semi donut chart
    * Remove paddings
    * Modify displayed text in tooltip
    * Remove outline for tooltip

--- src/utils/apiutil.js
+++ src/utils/apiutil.js
@@ -10,11 +10,11 @@ export async function getJsonDataFromUrl(url) {
 export function convertAllState(inlist) {
     // create a json looking string out of the Json list
     let conv_str = inlist.reduce((result, element) => {
-        return`${result}"${element.code}": "${element.name}", `
-    }, "")
+        return`${result}"${element.code}": "${element.name}", `;
+    }, "");
 
     // remove the very last space and comma
-    conv_str = "{ " + conv_str.substring(0, conv_str.length -2) + " }";
+    conv_str = `{ ${  conv_str.substring(0, conv_str.length -2)  } }`;
 
     return JSON.parse(conv_str);
 }
