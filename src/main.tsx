1a239b80620c2153c31367a836c45ea96148f20d
Ziyue Zhou <ziyue5@illinois.edu>
2023-04-17 04:06:47 -0500

    Add CSP Page

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import EQIPPage from "./pages/EQIPPage";
+import CSPPage from "./pages/CSPPage";
 
 const ScrollToTop = (props: any) => {
     const location = useLocation();
@@ -12,12 +13,13 @@ const ScrollToTop = (props: any) => {
     return <>{props.children}</>;
 };
 export default function Main(): JSX.Element {
-    return (
-        <ScrollToTop>
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/eqip" element={<EQIPPage />} />
-            </Routes>
-        </ScrollToTop>
-    );
+	return (
+		<ScrollToTop>
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/eqip" element={<EQIPPage />} />
+                <Route path="/csp" element={<CSPPage />} />
+			</Routes>
+		</ScrollToTop>
+	);
 }
--- src/pages/CSPPage.tsx
+++ src/pages/CSPPage.tsx
@@ -4,93 +4,102 @@ import { createTheme, ThemeProvider, Typography } from "@mui/material";
 import NavBar from "../components/NavBar";
 import Drawer from "../components/ProgramDrawer";
 import SemiDonutChart from "../components/SemiDonutChart";
-//import DataTable from "../components/EQIP/EQIPTotalTable";
-//import EQIPTotalMap from "../components/EQIP/EQIPTotalMap";
+import DataTable from "../components/CSP/CSPTotalTable";
+import CSPTotalMap from "../components/CSP/CSPTotalMap";
 import chartData from "../data/CSP/csp_practice_categories_data.json";
-//import CategoryTable from "../components/EQIP/CategoryTable";
-//import CategoryMap from "../components/EQIP/CategoryMap";
+import CategoryTable from "../components/CSP/CategoryTable";
+import CategoryMap from "../components/CSP/CategoryMap";
 
 export default function CSPPage(): JSX.Element {
 	const [checked, setChecked] = React.useState(0);
 
 	const defaultTheme = createTheme();
-	let structuralTotal = 0;
 	let landManagementTotal = 0;
+	let otherImprovementTotal = 0;
 	let vegetativeTotal = 0;
 	let forestManagementTotal = 0;
 	let soilRemediationTotal = 0;
-	let other6ATotal = 0;
-	let soilTestingTotal = 0;
+	let structuralTotal = 0;
 
-	let otherPlanningTotal = 0;
-	let conservationPlanningAssessmentTotal = 0;
-	let comprehensiveNutrientMgtTotal = 0;
-	let resourceConservingCropRotationTotal = 0;
-	let soilHealthTotal = 0;
+	let croplandTotal = 0;
+	let rangelandTotal = 0;
+	let pasturelandTotal = 0;
+	let NIPFTotal = 0;
+	let pasturedCroplandTotal = 0;
+
+	let otherPaymentsTotal = 0;
+	let bundlesTotal = 0;
 
 	// eslint-disable-next-line
     const cur1 = chartData.statutes.find((s) => s.statuteName === '(6)(A) Practices');
-	const cur2 = chartData.statutes.find((s) => s.statuteName === "(6)(B) Practices");
+	const cur2 = chartData.statutes.find((s) => s.statuteName === "2014 CSP");
+	const cur3 = chartData.statutes.find((s) => s.statuteName === "Other");
 	const sixATotal = cur1.totalPaymentInDollars;
-	const sixBTotal = cur2.totalPaymentInDollars;
+	const old2014Total = cur2.totalPaymentInDollars;
+	const otherTotal = cur3.totalPaymentInDollars;
 	const ACur = cur1.practiceCategories;
 	const BCur = cur2.practiceCategories;
+	const CCur = cur3.practiceCategories;
 
-	const structuralCur = ACur.find((s) => s.practiceCategoryName === "Structural");
 	const landManagementCur = ACur.find((s) => s.practiceCategoryName === "Land management");
+	const otherImprovementCur = ACur.find((s) => s.practiceCategoryName === "Other improvement");
 	const vegetativeCur = ACur.find((s) => s.practiceCategoryName === "Vegetative");
 	const forestManagementCur = ACur.find((s) => s.practiceCategoryName === "Forest management");
 	const soilRemediationCur = ACur.find((s) => s.practiceCategoryName === "Soil remediation");
-	const other6ACur = ACur.find((s) => s.practiceCategoryName === "Other improvement");
-	const soilTestingCur = ACur.find((s) => s.practiceCategoryName === "Soil testing");
+	const structuralCur = ACur.find((s) => s.practiceCategoryName === "Structural");
 
-	const otherPlanningCur = BCur.find((s) => s.practiceCategoryName === "Other planning");
-	const conservationPlanningAssessmentCur = BCur.find(
-		(s) => s.practiceCategoryName === "Conservation planning assessment"
-	);
-	const comprehensiveNutrientMgtCur = BCur.find((s) => s.practiceCategoryName === "Comprehensive Nutrient Mgt.");
-	const resourceConservingCropRotationCur = BCur.find(
-		(s) => s.practiceCategoryName === "Resource-conserving crop rotation"
-	);
-	const soilHealthCur = BCur.find((s) => s.practiceCategoryName === "Soil health");
+	const croplandCur = BCur.find((s) => s.practiceCategoryName === "Cropland");
+	const rangelandCur = BCur.find((s) => s.practiceCategoryName === "Rangeland");
+	const pasturelandCur = BCur.find((s) => s.practiceCategoryName === "Pastureland");
+	const NIPFCur = BCur.find((s) => s.practiceCategoryName === "NIPF");
+	const pasturedCroplandCur = BCur.find((s) => s.practiceCategoryName === "Pastured Cropland");
 
-	structuralTotal += Number(structuralCur.totalPaymentInDollars);
+	const otherPaymentsCur = CCur.find((s) => s.practiceCategoryName === "Other Payments");
+	const bundlesCur = CCur.find((s) => s.practiceCategoryName === "Bundles");
+
 	landManagementTotal += Number(landManagementCur.totalPaymentInDollars);
+	otherImprovementTotal += Number(otherImprovementCur.totalPaymentInDollars);
 	vegetativeTotal += Number(vegetativeCur.totalPaymentInDollars);
 	forestManagementTotal += Number(forestManagementCur.totalPaymentInDollars);
 	soilRemediationTotal += Number(soilRemediationCur.totalPaymentInDollars);
-	other6ATotal += Number(other6ACur.totalPaymentInDollars);
-	soilTestingTotal += Number(soilTestingCur.totalPaymentInDollars);
+	structuralTotal += Number(structuralCur.totalPaymentInDollars);
 
-	otherPlanningTotal += Number(otherPlanningCur.totalPaymentInDollars);
-	conservationPlanningAssessmentTotal += Number(conservationPlanningAssessmentCur.totalPaymentInDollars);
-	comprehensiveNutrientMgtTotal += Number(comprehensiveNutrientMgtCur.totalPaymentInDollars);
-	resourceConservingCropRotationTotal += Number(resourceConservingCropRotationCur.totalPaymentInDollars);
-	soilHealthTotal += Number(soilHealthCur.totalPaymentInDollars);
+	croplandTotal += Number(croplandCur.totalPaymentInDollars);
+	rangelandTotal += Number(rangelandCur.totalPaymentInDollars);
+	pasturelandTotal += Number(pasturelandCur.totalPaymentInDollars);
+	NIPFTotal += Number(NIPFCur.totalPaymentInDollars);
+	pasturedCroplandTotal += Number(pasturedCroplandCur.totalPaymentInDollars);
+
+	otherPaymentsTotal += Number(otherPaymentsCur.totalPaymentInDollars);
+	bundlesTotal += Number(bundlesCur.totalPaymentInDollars);
+
 
 	const sixAChartData = [
-		{ name: "Structural", value: structuralTotal, color: "#2F7164" },
-		{ name: "Land management", value: landManagementTotal, color: "#4D847A" },
+		{ name: "Land management", value: landManagementTotal, color: "#2F7164" },
+		{ name: "Other Improvement", value: otherImprovementTotal, color: "#4D847A" },
 		{ name: "Vegetative", value: vegetativeTotal, color: "#749F97" },
 		{ name: "Forest management", value: forestManagementTotal, color: "#9CBAB4" },
-		{ name: "Other improvement", value: other6ATotal, color: "#B9CDC9" },
-		{ name: "Soil remediation; Soil testing", value: soilRemediationTotal + soilTestingTotal, color: "#CDDBD8" }
+		{ name: "Soil remediation", value: soilRemediationTotal, color: "#B9CDC9" },
+		{ name: "Structural", value: structuralTotal, color: "#CDDBD8" }
 	];
 
-	const sixBChartData = [
-		{ name: "Other planning", value: otherPlanningTotal, color: "#2F7164" },
-		{ name: "Conservation planning assessment", value: conservationPlanningAssessmentTotal, color: "#4D847A" },
-		{ name: "Comprehensive Nutrient Mgt.", value: comprehensiveNutrientMgtTotal, color: "#749F97" },
-		{
-			name: "Resource-conserving crop rotation; Soil health",
-			value: resourceConservingCropRotationTotal + soilHealthTotal,
-			color: "#9CBAB4"
-		}
+	const old2014ChartData = [
+		{ name: "Cropland", value: croplandTotal, color: "#2F7164" },
+		{ name: "Rangeland", value: rangelandTotal, color: "#4D847A" },
+		{ name: "Pastureland", value: pasturelandTotal, color: "#749F97" },
+		{ name: "NIPF", value: NIPFTotal, color: "#9CBAB4" },
+		{ name: "Pastured Cropland", value: pasturedCroplandTotal, color: "#B9CDC9" },
+	];
+
+	const otherChartData = [
+		{ name: "Other Payments", value: otherPaymentsTotal, color: "#2F7164" },
+		{ name: "Bundles", value: bundlesTotal, color: "#4D847A" },
 	];
 
 	const totalChartData = [
 		{ name: "6 (A)", value: sixATotal, color: "#2F7164" },
-		{ name: "6 (B)", value: sixBTotal, color: "#9CBAB4" }
+		{ name: "2014 CSP", value: old2014Total, color: "#9CBAB4" },
+		{ name: "Other", value: otherTotal, color: "#749F97" },
 	];
 
 	return (
@@ -99,86 +108,85 @@ export default function CSPPage(): JSX.Element {
 				<Box sx={{ position: "fixed", zIndex: 1400, width: "100%" }}>
 					<NavBar />
 				</Box>
-				<Drawer setEQIPChecked={setChecked} />
 				<Box sx={{ pl: 50, pr: 20 }}>
 					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 0 ? "none" : "block" }}>
-						<EQIPTotalMap />
+						<CSPTotalMap />
 					</Box>
 					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 1 ? "none" : "block" }}>
 						<CategoryMap category="Land management" />
 					</Box>
 					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 2 ? "none" : "block" }}>
-						<CategoryMap category="Forest management" />
-					</Box>
-					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 3 ? "none" : "block" }}>
-						<CategoryMap category="Structural" />
-					</Box>
-					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 4 ? "none" : "block" }}>
-						<CategoryMap category="Soil remediation" />
-					</Box>
-					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 5 ? "none" : "block" }}>
-						<CategoryMap category="Vegetative" />
-					</Box>
-					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 6 ? "none" : "block" }}>
 						<CategoryMap category="Other improvement" />
 					</Box>
+					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 3 ? "none" : "block" }}>
+						<CategoryMap category="Vegetative" />
+					</Box>
+					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 4 ? "none" : "block" }}>
+						<CategoryMap category="Forest management" />
+					</Box>
+					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 5 ? "none" : "block" }}>
+						<CategoryMap category="Soil remediation" />
+					</Box>
+					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 6 ? "none" : "block" }}>
+						<CategoryMap category="Structural" />
+					</Box>
 					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 7 ? "none" : "block" }}>
-						<CategoryMap category="Soil testing" />
+						<CategoryMap category="Cropland" />
 					</Box>
 					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 8 ? "none" : "block" }}>
-						<CategoryMap category="Other planning" />
+						<CategoryMap category="Rangeland" />
 					</Box>
 					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 9 ? "none" : "block" }}>
-						<CategoryMap category="Conservation planning assessment" />
+						<CategoryMap category="Pastureland" />
 					</Box>
 					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 10 ? "none" : "block" }}>
-						<CategoryMap category="Resource-conserving crop rotation" />
+						<CategoryMap category="NIPF" />
 					</Box>
 					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 11 ? "none" : "block" }}>
-						<CategoryMap category="Soil health" />
+						<CategoryMap category="Pastured Cropland" />
 					</Box>
 					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 12 ? "none" : "block" }}>
-						<CategoryMap category="Comprehensive Nutrient Mgt." />
+						<CategoryMap category="Other Payments" />
+					</Box>
+					<Box component="div" sx={{ width: "85%", m: "auto", display: checked !== 13 ? "none" : "block" }}>
+						<CategoryMap category="Bundles" />
 					</Box>
 					<Box display="flex" justifyContent="center" flexDirection="column" sx={{ mt: 10, mb: 2 }}>
 						<Box display="flex" justifyContent="center">
 							<Typography variant="h5">
-								<strong>EQIP: State Performance by Category of Practices</strong>
+								<strong>CSP: State Performance by Category of Practices</strong>
 							</Typography>
 						</Box>
 						<Typography sx={{ mt: 2 }}>
-                            EQIP provides cost-share assistance for improvements to eligible land. In the statute,
-                            Congress defined seven categories of conservation practices: (1) structural practices, such
-                            as for irrigation and livestock manure management or abatement; (2) land management
-                            practices, such as for fencing, drainage water management, grazing, prescribed burning, and
-                            wildlife habitat; (3) vegetative practices, such as planting filter strips, cover crops,
-                            grassed waterways, field borders, windbreaks, and shelterbelts; (4) forest management
-                            practices, which include planting trees and shrubs, improving forest stands, planting
-                            riparian forest buffers, and treating residues; (5) soil testing practices; (6) soil
-                            remediation practices, such as residue and tillage management (no-till, mulch-till, or
-                            strip-till), and amendments for treating agricultural wastes; and (7) other practices,
-                            including integrated pest management, dust control, and energy improvements.
+                            CSP placeholder
 						</Typography>
 					</Box>
 					<Box component="div" sx={{ display: checked !== 0 ? "none" : "block" }}>
 						<SemiDonutChart
 							data={totalChartData}
-							label1={(sixATotal + sixBTotal).toString()}
-							label2="EQIP TOTAL BENEFITS"
+							label1={(sixATotal + old2014Total + otherTotal).toString()}
+							label2="CSP TOTAL BENEFITS"
 						/>
 					</Box>
-					<Box component="div" sx={{ display: checked >= 1 && checked <= 7 ? "block" : "none" }}>
+					<Box component="div" sx={{ display: checked >= 1 && checked <= 6 ? "block" : "none" }}>
 						<SemiDonutChart
 							data={sixAChartData}
 							label1={sixATotal.toString()}
 							label2="6(A) TOTAL BENEFITS"
 						/>
 					</Box>
-					<Box component="div" sx={{ display: checked >= 8 ? "block" : "none" }}>
+					<Box component="div" sx={{ display: checked >= 7 && checked <= 11 ? "block" : "none" }}>
 						<SemiDonutChart
-							data={sixBChartData}
-							label1={sixBTotal.toString()}
-							label2="6(B) TOTAL BENEFITS"
+							data={old2014ChartData}
+							label1={old2014Total.toString()}
+							label2="2014 CSP TOTAL BENEFITS"
+						/>
+					</Box>
+					<Box component="div" sx={{ display: checked >= 12 ? "block" : "none" }}>
+						<SemiDonutChart
+							data={otherChartData}
+							label1={otherTotal.toString()}
+							label2="Other TOTAL BENEFITS"
 						/>
 					</Box>
 					<Box display="flex" justifyContent="center" sx={{ mt: 10, mb: 2 }}>
@@ -193,37 +201,40 @@ export default function CSPPage(): JSX.Element {
 						<CategoryTable category="Land management" />
 					</Box>
 					<Box component="div" sx={{ display: checked !== 2 ? "none" : "block" }}>
-						<CategoryTable category="Forest management" />
-					</Box>
-					<Box component="div" sx={{ display: checked !== 3 ? "none" : "block" }}>
-						<CategoryTable category="Structural" />
-					</Box>
-					<Box component="div" sx={{ display: checked !== 4 ? "none" : "block" }}>
-						<CategoryTable category="Soil remediation" />
-					</Box>
-					<Box component="div" sx={{ display: checked !== 5 ? "none" : "block" }}>
-						<CategoryTable category="Vegetative" />
-					</Box>
-					<Box component="div" sx={{ display: checked !== 6 ? "none" : "block" }}>
 						<CategoryTable category="Other improvement" />
 					</Box>
+					<Box component="div" sx={{ display: checked !== 3 ? "none" : "block" }}>
+						<CategoryTable category="Vegetative" />
+					</Box>
+					<Box component="div" sx={{ display: checked !== 4 ? "none" : "block" }}>
+						<CategoryTable category="Forest management" />
+					</Box>
+					<Box component="div" sx={{ display: checked !== 5 ? "none" : "block" }}>
+						<CategoryTable category="Soil remediation" />
+					</Box>
+					<Box component="div" sx={{ display: checked !== 6 ? "none" : "block" }}>
+						<CategoryTable category="Structural" />
+					</Box>
 					<Box component="div" sx={{ display: checked !== 7 ? "none" : "block" }}>
-						<CategoryTable category="Soil testing" />
+						<CategoryTable category="Cropland" />
 					</Box>
 					<Box component="div" sx={{ display: checked !== 8 ? "none" : "block" }}>
-						<CategoryTable category="Other planning" />
+						<CategoryTable category="Rangeland" />
 					</Box>
 					<Box component="div" sx={{ display: checked !== 9 ? "none" : "block" }}>
-						<CategoryTable category="Conservation planning assessment" />
+						<CategoryTable category="Pastureland" />
 					</Box>
 					<Box component="div" sx={{ display: checked !== 10 ? "none" : "block" }}>
-						<CategoryTable category="Resource-conserving crop rotation" />
+						<CategoryTable category="NIPF" />
 					</Box>
 					<Box component="div" sx={{ display: checked !== 11 ? "none" : "block" }}>
-						<CategoryTable category="Soil health" />
+						<CategoryTable category="Pastured Cropland" />
 					</Box>
 					<Box component="div" sx={{ display: checked !== 12 ? "none" : "block" }}>
-						<CategoryTable category="Comprehensive Nutrient Mgt." />
+						<CategoryTable category="Other Payments" />
+					</Box>
+					<Box component="div" sx={{ display: checked !== 12 ? "none" : "block" }}>
+						<CategoryTable category="Bundles" />
 					</Box>
 				</Box>
 			</Box>
