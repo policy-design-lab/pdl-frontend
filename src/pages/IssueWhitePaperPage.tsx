93ebc5c8bba050966c7ed470ba0187460c24f10c
Wendy(Pengyin) Shan <wendy.shan@hotmail.com>
2024-05-08 15:13:56 -0500

    #278 add anchor as a temporary solution to the distinuct url for issue/paper items

--- src/pages/IssueWhitePaperPage.tsx
+++ src/pages/IssueWhitePaperPage.tsx
@@ -9,6 +9,7 @@ import CardPaper from "../components/issueWhitePaper/cardPaper";
 import KnowTheScore from "../files/issues/Know_the_Score.pdf";
 
 export default function IssueWhitePaperPage(): JSX.Element {
+    const [cardId, setCardId] = React.useState("");
     const [tab, setTab] = React.useState(0);
 
     const defaultTheme = createTheme({
@@ -22,6 +23,31 @@ export default function IssueWhitePaperPage(): JSX.Element {
     const iframeWidth = window.innerWidth * 0.9;
     const iframeHeight = window.innerWidth > 1679 ? window.innerHeight * 0.92 : window.innerHeight * 0.95;
 
+    React.useEffect(() => {
+        if (window.location.hash) {
+            const hashId = window.location.hash.substring(1);
+            setCardId(hashId);
+        }
+    }, []);
+    React.useEffect(() => {
+        if (cardId) {
+            if (cardId.includes("paper")) setTab(1);
+            else setTab(0);
+            const element = document.getElementById(cardId);
+            if (element) {
+                let offsetPosition = 0;
+                if (cardId.includes("paper")) {
+                    const offset = 20;
+                    const elementPosition = element.getBoundingClientRect().top;
+                    offsetPosition = elementPosition - offset;
+                }
+                window.scrollTo({
+                    top: offsetPosition,
+                    behavior: "smooth"
+                });
+            }
+        }
+    }, [cardId]);
     return (
         <ThemeProvider theme={defaultTheme}>
             <Box sx={{ width: "100%" }}>
@@ -119,6 +145,7 @@ export default function IssueWhitePaperPage(): JSX.Element {
                                             sx={{ display: "flex", width: "100%" }}
                                         >
                                             <CardIFrame
+                                                id="issue-what-farmers-stand"
                                                 title="ISSUE BRIEF: What Farmers Stand to Lose in the Farm Bill If Congress Eliminates Conservation Investments"
                                                 iframeTitle="What Farmers Stand to Lose in the Farm Bill If Congress Eliminates Conservation Investments"
                                                 author="Professor Jonathan Coppess, Policy Design Lab, University of Illinois"
@@ -148,6 +175,7 @@ export default function IssueWhitePaperPage(): JSX.Element {
                                             sx={{ display: "flex", width: "100%" }}
                                         >
                                             <CardPaper
+                                                id="paper-know-the-score"
                                                 title="Know the Score: The Hidden Costs of Repurposing Farm Conservation Investments"
                                                 description="In the pending farm bill reauthorization, potential efforts to repurpose recent investments in farm conservation programs are plagued by hidden costs from obscure budget rules that risk farmers losing much, if not all, of the investments; this white paper discusses those hidden costs and the risks to conservation assistance to farmers."
                                                 author="Professor Jonathan Coppess, Policy Design Lab, University of Illinois"
