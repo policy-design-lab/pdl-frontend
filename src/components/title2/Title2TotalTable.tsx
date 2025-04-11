fe379a54bdf193063041162b2e43a75b30edfc0f
Ya-Lan Yang <ylyang@illinois.edu>
2025-04-11 16:14:16 -0500

    Update Title2TotalTable.tsx

--- src/components/title2/Title2TotalTable.tsx
+++ src/components/title2/Title2TotalTable.tsx
@@ -111,6 +111,7 @@ function Table({ columns, data }: { columns: any; data: any }) {
                                     <Box sx={{ display: "flex", alignItems: "center" }}>
                                         {column.render("Header")}
                                         <div>
+                                          {/* eslint-disable-next-line no-nested-ternary */}
                                             {!column.isSorted ? (
                                                 <Box sx={{ ml: 1 }}>
                                                     <SwapVertIcon />
