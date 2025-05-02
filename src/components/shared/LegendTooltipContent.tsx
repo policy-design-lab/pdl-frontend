7ccff0c05573b37013dcd016c962d18aa0d3cf62
Wendy(Pengyin) Shan <wendy.shan@hotmail.com>
2025-05-02 10:31:20 -0400

    fix build error

--- src/components/shared/LegendTooltipContent.tsx
+++ src/components/shared/LegendTooltipContent.tsx
@@ -31,12 +31,14 @@ export const LegendTooltipContent: React.FC<LegendTooltipContentProps> = ({
             </div>
         <div style={{ marginBottom: "8px", lineHeight: "1.4", fontSize: "0.9rem" }}>
           {tooltipData.regionCount > 0 ? (
-          `${tooltipData.regionCount} ${regionPlural} with ${valueType} that fall between the ${startPercentile}th and ${endPercentile}th percentiles of all values`
-                    : isHighestRange
-                    ? `This represents the highest value range (${startPercentile}%-${endPercentile}%). There are no ${regionPlural} with values in this specific range.`
-                    : `There are no ${regionPlural} with ${valueType} that fall between the ${startPercentile}th and ${endPercentile}th percentiles.`}
-            </div>
-        {(tooltipData.regionCount > 0 || hasHighestValueInfo) && 
+            `${tooltipData.regionCount} ${regionPlural} with ${valueType} that fall between the ${startPercentile}th and ${endPercentile}th percentiles of all values`
+          ) : isHighestRange ? (
+            `This represents the highest value range (${startPercentile}%-${endPercentile}%). There are no ${regionPlural} with values in this specific range.`
+          ) : (
+            `There are no ${regionPlural} with ${valueType} that fall between the ${startPercentile}th and ${endPercentile}th percentiles.`
+          )}
+          </div>
+          {(tooltipData.regionCount > 0 || hasHighestValueInfo) &&
        isRegionObject(tooltipData.minRegion) && 
        isRegionObject(tooltipData.maxRegion) && (
        <>
