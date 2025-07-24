99b79e209475f6b9b4cbc7c0b05ed6ff0e3cdf2f
Wendy(Pengyin) Shan <wendy.shan@hotmail.com>
2025-07-24 16:37:27 -0400

    fix linting problem

--- src/components/shared/TableCompareFunctions.tsx
+++ src/components/shared/TableCompareFunctions.tsx
@@ -1,17 +1,14 @@
 export function compareWithDollarSign(rowA: any, rowB: any, id: string): number {
     const aVal = rowA.values[id];
     const bVal = rowB.values[id];
-
     let a = 0;
     let b = 0;
-
     try {
         if (typeof aVal === "string" && aVal.startsWith("$")) {
             a = Number.parseFloat(aVal.substring(1).replaceAll(",", ""));
         } else if (typeof aVal === "number") {
             a = aVal;
         }
-
         if (typeof bVal === "string" && bVal.startsWith("$")) {
             b = Number.parseFloat(bVal.substring(1).replaceAll(",", ""));
         } else if (typeof bVal === "number") {
@@ -24,28 +21,22 @@ export function compareWithDollarSign(rowA: any, rowB: any, id: string): number 
         a = 0;
         b = 0;
     }
-
     if (Number.isNaN(a)) a = 0;
     if (Number.isNaN(b)) b = 0;
-
     const comparison = a - b;
-
     return comparison;
 }
 
 export function compareWithPercentSign(rowA: any, rowB: any, id: string): number {
     const a = Number.parseFloat(rowA.values[id].replaceAll("%", ""));
     const b = Number.parseFloat(rowB.values[id].replaceAll("%", ""));
-
     return a - b;
 }
 
 export function compareWithAlphabetic(rowA: any, rowB: any, id: string): number {
     const a = rowA.values[id] || "";
     const b = rowB.values[id] || "";
-
     const comparison = a.localeCompare(b);
-
     return comparison;
 }
 
@@ -60,13 +51,10 @@ export function sortByDollars(jsonArray: any[], key: string): void {
 export function compareWithNumber(rowA: any, rowB: any, id: string): number {
     const aVal = rowA.values[id];
     const bVal = rowB.values[id];
-
     if (aVal === undefined || aVal === null) return 1;
     if (bVal === undefined || bVal === null) return -1;
-
     let a;
     let b;
-
     if (typeof aVal === "number") {
         a = aVal;
     } else if (typeof aVal === "string") {
@@ -74,7 +62,6 @@ export function compareWithNumber(rowA: any, rowB: any, id: string): number {
     } else {
         a = 0;
     }
-
     if (typeof bVal === "number") {
         b = bVal;
     } else if (typeof bVal === "string") {
@@ -82,9 +69,7 @@ export function compareWithNumber(rowA: any, rowB: any, id: string): number {
     } else {
         b = 0;
     }
-
     if (Number.isNaN(a)) a = 0;
     if (Number.isNaN(b)) b = 0;
-
     return a - b;
 }
